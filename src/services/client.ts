aa00414fa08587e05d8c73bf561a4cade054bcfc
VVicheka <vann.vicheka.2822@rupp.edu.kh>
2026-05-03 13:25:36 +0700

    Revert "proxy api"
    This reverts commit 054afcf8bfbfb638d2e4f71f8fd687e08dbfe102.

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+
 const client = axios.create({
-  baseURL: "",
+  baseURL: API_BASE_URL,
   headers: {
     "Accept": "application/json",
   },
